1cb9af6ddf3fa3cc42f7c058e78567d0908d3551
Abuali223 <kamerasony24@gmail.com>
2025-08-20 00:19:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-window.SUPABASE_URL = "https://<YOUR-PROJECT>.supabase.co"; // masalan: https://abcde12345.supabase.co
-window.SUPABASE_ANON_KEY = "<YOUR_ANON_PUBLIC_KEY>";        // Dashboard → Settings → API → anon public key
+window.SUPABASE_URL = "https://https://abcde12345.supabase.co"; // masalan: 
+window.SUPABASE_ANON_KEY = "<anon public key>";        // Dashboard → Settings → API → 
