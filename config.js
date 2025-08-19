4ba471c210457b51d12abe451d39cf152315c972
Abuali223 <kamerasony24@gmail.com>
2025-08-20 02:20:37 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // 1) Project URL (faqat .supabase.co gacha!  /functions/... QO‘YILMAYDI)
 window.SUPABASE_URL = "https://eulfjekrjdtprffszxcf.supabase.co";
 
-// 2) anon public key (service_role EMAS)
-window.SUPABASE_ANON_KEY = "<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1bGZqZWtyamR0cHJmZnN6eGNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MzE4NjgsImV4cCI6MjA3MTIwNzg2OH0.TFjqVweIZWg3xAjA45wExOzFZ10DplSO3TzZmvUlLGo'>";
+// 2) anon public key (service_role EMAS!)
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1bGZqZWtyamR0cHJmZnN6eGNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MzE4NjgsImV4cCI6MjA3MTIwNzg2OH0.TFjqVweIZWg3xAjA45wExOzFZ10DplSO3TzZmvUlLGo";
