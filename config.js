3bec55939abb52a6b022a8b1f002cd6265d44f15
Abuali223 <kamerasony24@gmail.com>
2025-08-20 02:04:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
+// === Supabase Frontend sozlamalari ===
+// Project Settings → API sahifasidan olingan qiymatlar:
+
+// 1) Project URL (faqat .supabase.co gacha!  /functions/... QO‘YILMAYDI)
 window.SUPABASE_URL = "https://eulfjekrjdtprffszxcf.supabase.co";
-window.SUPABASE_ANON_KEY = "<anon public key>";
+
+// 2) anon public key (service_role EMAS)
+window.SUPABASE_ANON_KEY = "<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1bGZqZWtyamR0cHJmZnN6eGNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MzE4NjgsImV4cCI6MjA3MTIwNzg2OH0.TFjqVweIZWg3xAjA45wExOzFZ10DplSO3TzZmvUlLGo>";
