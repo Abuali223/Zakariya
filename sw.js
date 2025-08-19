77ffd426d1628e3e4ed8bf549ec8e85473d90398
Abuali223 <kamerasony24@gmail.com>
2025-08-19 23:44:41 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,21 @@
-const CACHE = 'arab-pwa-v1';
+const CACHE = 'arab-pwa-v2'; // <-- yangilandi
 const CORE = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json',
-  './assets/icons/icon-192.png',
-  './assets/icons/icon-512.png'
+  './','./index.html','./app.js','./ai.js',
+  './manifest.json','./assets/icons/icon-192.png','./assets/icons/icon-512.png'
 ];
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(
-    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-  )));
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e=>{
   const url = new URL(e.request.url);
   if (e.request.mode === 'navigate') {
-    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));
-    return;
+    e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html'))); return;
   }
   if (url.origin === location.origin) {
     e.respondWith(
